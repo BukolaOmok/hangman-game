218b361e1da29ec38f662f660b001443d4233bab
2405-005-bo <omokehindeoluwabukola@gmail.com>
2024-06-24 22:17:09 +0100

    update code

--- src/Hangman.jsx
+++ src/Hangman.jsx
@@ -5,40 +5,58 @@ import letterData from "./letterData";
 export default function HangmanGame() {
   const actualWord = "bukola";
   const splitWordToGuess = actualWord.split("");
-
+  const maxMisses = actualWord.length + 5
   const [wordToGuess, setWordToGuess] = React.useState(
     splitWordToGuess.map(() => "_ ")
   );
   const [guessedLetters, setGuessedLetters] = React.useState([]);
   const [missesCount, setMissesCount] = React.useState(0);
 
-  const updateGuessedLettersArray = (letter) => {
-    setGuessedLetters((prevGuessedLetters) => [...prevGuessedLetters, letter]);
+  // const updateGuessedLettersArray = (letter) => {
+  //   setGuessedLetters(guessedLetters => [...guessedLetters, letter]);
 
-    if (!splitWordToGuess.includes(letter)) {
-      setMissesCount((missesCount) => missesCount + 1);
-    } else {
-      revealGuessedLetters(letter);
-    }
-  };
+  //   if (!splitWordToGuess.includes(letter)) {
+  //     setMissesCount(missesCount => missesCount + 1);
+  //   } else {
+  //     revealGuessedLetters(letter);
+  //   }
+  // };
 
   const revealGuessedLetters = (letter) => {
-    const revealedWord = splitWordToGuess.map((l, index) => {
-      if (guessedLetters.includes(l) || l === letter) {
-        return l;
+    const revealedWord = wordToGuess.map((char, index) => {
+      if (splitWordToGuess[index] === letter || guessedLetters.includes(splitWordToGuess[index])) {
+        return splitWordToGuess[index];
       } else {
-        return "_";
+        return char;
       }
     });
     setWordToGuess(revealedWord);
   };
 
-  const handleGuesses = (letter) => {
+  const handleGuesses = (letter) => () => {
     if (!guessedLetters.includes(letter)) {
-      updateGuessedLettersArray(letter);
+    const newGuessedLetters = [...guessedLetters, letter];
+    setGuessedLetters(newGuessedLetters);
+    if (actualWord.includes(letter)) {
+      revealGuessedLetters(letter, newGuessedLetters);
+    } else {
+      updateMissesCount();
     }
+  }
+};
+
+const updateMissesCount = () => {
+  setMissesCount(missesCount => missesCount + 1);
+  if (missesCount > maxMisses) {
+  setGameStatus('You lose! Too many misses');
+  }
   };
 
+  const winGameStatus = (revealedWord) => {
+    if (!revealedWord.includes('_')) {
+    setGameStatus('You win!');
+    }
+    };
 
   return (
     <div className="group-content">
@@ -47,12 +65,17 @@ export default function HangmanGame() {
       <p className="misses-count-style">number of misses: {missesCount}</p>
 
       <div className="group-letter">
-        {letterData.map((letter =>
-      <button key={letter} className="letter" onClick={handleGuesses(letter)}>
-      {letter}
-    </button>
-    ))}
-    </div>
+        {letterData.map((letter) => (
+          <button
+            key={letter}
+            className="letter"
+            onClick={handleGuesses(letter)}
+            disabled = {guessedLetters.includes(letter)}
+          >
+            {letter}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
